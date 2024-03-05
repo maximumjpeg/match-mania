b13f30fe0d27a9d04f717eca4aa41c99a595fba2
Jordan Addleman <xibalbarising@gmail.com>
2024-03-04 22:30:09 -0500

    set up an expressJS server to serve the static site

--- src/landing.jsx
+++ src/landing.jsx
@@ -4,6 +4,7 @@ const Landing = (props) => {
   return <div className="landing-wrapper">
     <h1>Welcome to Match Mania!</h1>
     <h1>🏯🇯🇵🇯🇵🇯🇵🏯🇯🇵🇯🇵🇯🇵🏯🇯🇵🇯🇵🇯🇵🏯🇯🇵🇯🇵🇯🇵🏯</h1>
+    <h5>Practice studying Kana</h5>
     <p>Log in here</p>
     <button onClick={() => props.setLoggedIn(true)}>LOG IN</button>
   </div>
