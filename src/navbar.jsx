1b4426c42cb664231d7d324a07ca7de130f26c6e
Jordan Addleman <xibalbarising@gmail.com>
2024-03-06 23:21:41 -0500

    Nav Bar renders, no functionality yet...

---
+++ src/navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NavBar = (props) => {
+  return <div className="navbar">
+    {/* <a>Home</a>
+    <a>Gallery</a>
+    <a>Game Mode</a> */}
+    <p>Home</p>
+    <p>Gallery</p>
+    <p>Game Mode</p>
+  </div>
+}
+
+export default NavBar
--- src/parent.jsx
+++ src/parent.jsx
@@ -3,12 +3,14 @@ import React, { useState } from 'react';
 import Gallery from './gallery.jsx';
 import View from './view.jsx';
 import Landing from './landing.jsx';
+import NavBar from './navbar.jsx';
 
 const Parent = () => {
   const [loggedIn, setLoggedIn] = useState('false');
   const [current, setCurrent] = useState('');
   if (loggedIn && loggedIn === true) {
   return <div className="app-wrapper">
+    <NavBar />
     <h1>Match Mania</h1>
     <View current={current} />
     <Gallery setCurrent={setCurrent} />
