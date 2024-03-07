0af4fefc423eb262c775caf5cd1efd13c0668be9
Jordan Addleman <xibalbarising@gmail.com>
2024-03-06 22:06:22 -0500

    added full hiragana and katakana

--- src/landing.jsx
+++ src/landing.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 const Landing = (props) => {
   return <div className="landing-wrapper">
     <h1>Welcome to Match Mania!</h1>
-    <h1>🏯🇯🇵🇯🇵🇯🇵🏯🇯🇵🇯🇵🇯🇵🏯🇯🇵🇯🇵🇯🇵🏯🇯🇵🇯🇵🇯🇵🏯</h1>
+    <h1 className="big-emoji">🏯🇯🇵🇯🇵🏯</h1>
     <h5>Practice studying Kana</h5>
     <p>Log in here</p>
-    <button onClick={() => props.setLoggedIn(true)}>LOG IN</button>
+    <button onClick={() => {props.setLoggedIn(true)}}>LOG IN</button>
   </div>
 }
 
--- src/parent.jsx
+++ src/parent.jsx
@@ -7,19 +7,18 @@ import Landing from './landing.jsx';
 const Parent = () => {
   const [loggedIn, setLoggedIn] = useState('false');
   const [current, setCurrent] = useState('');
-  if (!loggedIn) {
-    return <Landing setLoggedIn={setLoggedIn} />
-  } else if (loggedIn) {
-    return <div className="app-wrapper">
-    <Landing setLoggedIn={setLoggedIn} />
+  if (loggedIn && loggedIn === true) {
+  return <div className="app-wrapper">
+    <h1>Match Mania</h1>
+    <View current={current} />
+    <Gallery setCurrent={setCurrent} />
   </div>
+  } else {
+    return <div className="app-wrapper">
+      <Landing setLoggedIn={setLoggedIn} />
+    </div>
   }
 };
 
 export default Parent
 
-// return <div className="app-wrapper">
-// <h1>Match Mania</h1>
-// <View current={current} />
-// <Gallery setCurrent={setCurrent} />
-// </div>
