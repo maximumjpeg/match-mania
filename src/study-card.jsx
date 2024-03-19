55a4ba9208ece96c5c9093928ad0b69d816834cc
Jordan Addleman <xibalbarising@gmail.com>
2024-03-19 19:42:28 -0400

    created study-card and study mode components in src

---
+++ src/study-card.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StudyCard = (props) => {
+  return <div className="study-card">
+    Study Card
+  </div>
+}
+
+export default StudyCard
---
+++ src/study.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Study = (props) => {
+  return <div className="study-wrapper">
+    Study Mode
+  </div>
+}
+
+export default Study
